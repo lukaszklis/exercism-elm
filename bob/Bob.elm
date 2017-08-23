0d6ed54d3c774f5722ef93720fc9c5d82526bfbf
Lukasz Klis <lukasz@klis.pl>
2017-08-23 17:58:58 +0200

    :sparkles: Add solution for ”Bob” task

--- bob/Bob.elm
+++ bob/Bob.elm
@@ -1,1 +1,35 @@
-module Bob exposing (..)
+module Bob exposing (hey)
+
+import Regex
+
+
+isEmpty : String -> Bool
+isEmpty msg =
+    String.isEmpty (String.trim msg)
+
+
+hasLetters : String -> Bool
+hasLetters msg =
+    Regex.contains (Regex.regex "[a-zA-Z]") msg
+
+
+isShouting : String -> Bool
+isShouting msg =
+    hasLetters msg && String.toUpper msg == msg
+
+
+isQuestion : String -> Bool
+isQuestion msg =
+    String.endsWith "?" msg
+
+
+hey : String -> String
+hey msg =
+    if isEmpty msg then
+        "Fine. Be that way!"
+    else if isShouting msg then
+        "Whoa, chill out!"
+    else if isQuestion msg then
+        "Sure."
+    else
+        "Whatever."
